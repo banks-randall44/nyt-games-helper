00b539df2237069becaa5121ca7b75f427739f2a
Randall Banks <banks.randall44@gmail.com>
2025-04-15 00:47:22 -0700

    Auto focus first tile on page load

--- src/app/wordle/_components/WordGrid.jsx
+++ src/app/wordle/_components/WordGrid.jsx
@@ -1,116 +1,66 @@
 import { View, Text, TextInput } from 'react-native-web'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import style from '../style.js'
 
-
 // Tile Component
-const LetterTile = ({value}) => {
+const LetterTile = ({ row, col }) => {
+    let [value, setValue] = useState('')
+    const [focusTarget, setFocusTarget] = useState([0,0])
+
+
+    const handleChangeText = (text) => {
+        // Update focus target
+        // If text is empty, go back a tile. Otherwise, go forward
+        // TODO
+
+        setValue(text)
+    }
+
+    // Focus the top left tile
+    let autoFocus = false
+    if (row == focusTarget[0] && col == focusTarget[1]) {
+        console.log('focusing on ('+row+','+col+')')
+        autoFocus = true
+    }
+
     return (
         <View style={style.letterTile}>
             <TextInput
+                autoFocus={autoFocus}
                 style={style.letterInput}
                 maxLength={1}
-                value={value}
+                value={value.toUpperCase()}
+                onChangeText={handleChangeText}
             />
         </View>
     )
 }
 
 // Word entry component
-const WordEntry = ({word}) => {
+const WordEntry = ({ row }) => {
     return (
         <View style={style.wordEntry}>
-            <LetterTile value={word[0]} />
-            <LetterTile value={word[1]} />
-            <LetterTile value={word[2]} />
-            <LetterTile value={word[3]} />
-            <LetterTile value={word[4]} />
+            <LetterTile row={row} col={0} />
+            <LetterTile row={row} col={1} />
+            <LetterTile row={row} col={2} />
+            <LetterTile row={row} col={3} />
+            <LetterTile row={row} col={4} />
         </View>
     )
 }
 
 // Word grid component
 const WordGrid = () => {
-    const [gridValues, setGridValues] = useState([
-        ['','','','',''],
-        ['','','','',''],
-        ['','','','',''],
-        ['','','','',''],
-        ['','','','',''],
-        ['','','','','']
-    ])
-
     return (
         <View style={style.gridContainer}>
-            <WordEntry 
-                word={gridValues[0]}
-            />
-            <WordEntry 
-                word={gridValues[1]}
-            />
-            <WordEntry 
-                word={gridValues[2]}
-            />
-            <WordEntry 
-                word={gridValues[3]}
-            />
-            <WordEntry 
-                word={gridValues[4]}
-            />
-            <WordEntry 
-                word={gridValues[5]}
-            />
+            <WordEntry row={0} />
+            <WordEntry row={1} />
+            <WordEntry row={2} />
+            <WordEntry row={3} />
+            <WordEntry row={4} />
+            <WordEntry row={5} />
         </View>
     )
 }
-
-
-// Alternate approach where LetterTiles simple remember their own values
-// Its not yet clear how I would go about fetching those values for word processing 
-//
-// Tile Component
-//const LetterTile = () => {
-//    const [value, setValue] = useState('')
-//
-//    return (
-//        <View style={style.letterTile}>
-//            <TextInput
-//                style={style.letterInput}
-//                maxLength={1}
-//                value={value}
-//                onChangeText={setValue}
-//            />
-//        </View>
-//    )
-//}
-//
-//// Word entry component
-//const WordEntry = () => {
-//    return (
-//        <View style={style.wordEntry}>
-//            <LetterTile />
-//            <LetterTile />
-//            <LetterTile />
-//            <LetterTile />
-//            <LetterTile />
-//        </View>
-//    )
-//}
-//
-//// Word grid component
-//const WordGrid = () => {
-//    return (
-//        <View style={style.gridContainer}>
-//            <WordEntry />
-//            <WordEntry />
-//            <WordEntry />
-//            <WordEntry />
-//            <WordEntry />
-//            <WordEntry />
-//        </View>
-//    )
-//}
-
-
 
 export default WordGrid
---
+++ src/app/wordle/loading.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native-web'
+
+const Loading = () => {
+    console.log('Spelling bee loading() invoked')
+    return (
+        <View>
+            <Text>
+                Loading...
+            </Text>
+        </View>
+    )
+}
+
+export default Loading
