4b4b2f7d4a833b352be28d2c8f1d1ed7e9f23a95
Randall Banks <banks.randall44@gmail.com>
2025-04-14 22:28:48 -0700

    End of night backup

--- src/app/wordle/_components/WordGrid.jsx
+++ src/app/wordle/_components/WordGrid.jsx
@@ -1,33 +1,116 @@
 import { View, Text, TextInput } from 'react-native-web'
+import { useState } from 'react'
 import style from '../style.js'
 
+
 // Tile Component
-const LetterTile = ({value}) => (
-    <View style={style.letterTile}>
-        <TextInput
-            style={style.letterInput}
-            maxLength={1}
-            value={value}
-        />
-    </View>
-)
-let tiles = []
-for (var i = 0; i < 5; i++) tiles.push(<LetterTile value={''} key={i} />)
+const LetterTile = ({value}) => {
+    return (
+        <View style={style.letterTile}>
+            <TextInput
+                style={style.letterInput}
+                maxLength={1}
+                value={value}
+            />
+        </View>
+    )
+}
 
 // Word entry component
-const WordEntry = () => (
-    <View style={style.wordEntry}>
-        {tiles}
-    </View>
-)
-let wordEntries = []
-for (var i = 0; i < 6; i++) wordEntries.push(<WordEntry key={i} />)
+const WordEntry = ({word}) => {
+    return (
+        <View style={style.wordEntry}>
+            <LetterTile value={word[0]} />
+            <LetterTile value={word[1]} />
+            <LetterTile value={word[2]} />
+            <LetterTile value={word[3]} />
+            <LetterTile value={word[4]} />
+        </View>
+    )
+}
 
 // Word grid component
-const WordGrid = () => (
-    <View style={style.gridContainer}>
-        {wordEntries}
-    </View>
-)
+const WordGrid = () => {
+    const [gridValues, setGridValues] = useState([
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','',''],
+        ['','','','','']
+    ])
+
+    return (
+        <View style={style.gridContainer}>
+            <WordEntry 
+                word={gridValues[0]}
+            />
+            <WordEntry 
+                word={gridValues[1]}
+            />
+            <WordEntry 
+                word={gridValues[2]}
+            />
+            <WordEntry 
+                word={gridValues[3]}
+            />
+            <WordEntry 
+                word={gridValues[4]}
+            />
+            <WordEntry 
+                word={gridValues[5]}
+            />
+        </View>
+    )
+}
+
+
+// Alternate approach where LetterTiles simple remember their own values
+// Its not yet clear how I would go about fetching those values for word processing 
+//
+// Tile Component
+//const LetterTile = () => {
+//    const [value, setValue] = useState('')
+//
+//    return (
+//        <View style={style.letterTile}>
+//            <TextInput
+//                style={style.letterInput}
+//                maxLength={1}
+//                value={value}
+//                onChangeText={setValue}
+//            />
+//        </View>
+//    )
+//}
+//
+//// Word entry component
+//const WordEntry = () => {
+//    return (
+//        <View style={style.wordEntry}>
+//            <LetterTile />
+//            <LetterTile />
+//            <LetterTile />
+//            <LetterTile />
+//            <LetterTile />
+//        </View>
+//    )
+//}
+//
+//// Word grid component
+//const WordGrid = () => {
+//    return (
+//        <View style={style.gridContainer}>
+//            <WordEntry />
+//            <WordEntry />
+//            <WordEntry />
+//            <WordEntry />
+//            <WordEntry />
+//            <WordEntry />
+//        </View>
+//    )
+//}
+
+
 
 export default WordGrid
